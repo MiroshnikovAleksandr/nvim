055cc67ee25c00a8f48f43b4299cff2263ff5a04
a.miroshnikov <alexmoff04@yandex.ru>
2026-08-14 17:31:13 +0300

    Tweak lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -23,8 +23,8 @@ require("lualine").setup({
     options = {
         theme = "auto",
         icons_enabled = true,
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         disabled_filetypes = {
             statusline = {},
             winbar = {},
